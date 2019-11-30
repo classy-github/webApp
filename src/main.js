ec7ca04c05bebe03d09a9de5dd14f89d9d8e6827
wanglintao <wanglintao@fast4ward.cn>
2019-11-30 20:16:51 +0800

    项目初始化-rem适配

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-// 引入加载执行注册模块
+// 引入加载执行注册Vant模块
 import './utils/register-vant'
+
+// 引入amfe-flexible动态设置rem基准值
+import 'amfe-flexible'
+
 new Vue({
   router,
   store,
